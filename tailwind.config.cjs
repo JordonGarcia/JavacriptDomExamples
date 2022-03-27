df2842779761415039b9b4ca406ddfb7140b9c93
Jordon <jordontgarcia@gmail.com>
2022-03-27 03:32:21 -0700

    Added custom Tailwind media breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,18 @@
 module.exports = {
-	content: [
-		'./index.html',
-		'./src/**/*.{vue,js,ts,jsx,tsx}',
-	],
+	content: ['./index.html', './src/projects/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				tablet: '640px',
+				// => @media (min-width: 640px) { ... }
+
+				laptop: '1024px',
+				// => @media (min-width: 1024px) { ... }
+
+				desktop: '1280px',
+				// => @media (min-width: 1280px) { ... }
+			},
+		},
 	},
 	plugins: [],
 };
