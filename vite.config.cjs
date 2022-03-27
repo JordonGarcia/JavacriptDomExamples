b586d120877aac33e3d8d66f7e546d8a7c0d38c2
Jordon <jordontgarcia@gmail.com>
2022-03-27 04:08:42 -0700

    Changed list of projects to forEach method

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,17 +1,18 @@
 const { resolve } = require('path');
 const { defineConfig } = require('vite');
 
+const projects = ['projectA', 'projectB', 'projectC', 'projectD', 'projectE'];
+
 module.exports = defineConfig({
 	build: {
 		outDir: 'build',
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, 'index.html'),
-				projectA: resolve(__dirname, 'src/projects/projectA.html'),
-				projectB: resolve(__dirname, 'src/projects/projectB.html'),
-				projectC: resolve(__dirname, 'src/projects/projectC.html'),
-				projectD: resolve(__dirname, 'src/projects/projectD.html'),
-				projectE: resolve(__dirname, 'src/projects/projectE.html'),
+
+				...projects.forEach((project) => ({
+					[project]: resolve(__dirname, `src/projects/${project}.html`),
+				})),
 			},
 		},
 	},
