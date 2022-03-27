da34d2a4c884008851c176b0e0ce95d01723c999
Jordon <jordontgarcia@gmail.com>
2022-03-27 04:22:03 -0700

    Switched back to previous file to fix issue

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 const { defineConfig } = require('vite');
 
-const projects = ['projectA', 'projectB', 'projectC', 'projectD', 'projectE'];
+// const projects = ['projectA', 'projectB', 'projectC', 'projectD', 'projectE'];
 
 module.exports = defineConfig({
 	build: {
@@ -10,9 +10,17 @@ module.exports = defineConfig({
 			input: {
 				main: resolve(__dirname, 'index.html'),
 
-				...projects.forEach((project) => ({
-					[project]: resolve(__dirname, `src/projects/${project}.html`),
-				})),
+				// TODO: Fix the commented code. Error: [vite:resolve] id.startsWith is not a function
+
+				// ...projects.map((project) => ({
+				// 	[project]: resolve(__dirname, `src/projects/${project}.html`),
+				// })),
+
+				projectA: resolve(__dirname, 'src/projects/projectA.html'),
+				projectB: resolve(__dirname, 'src/projects/projectB.html'),
+				projectC: resolve(__dirname, 'src/projects/projectC.html'),
+				projectD: resolve(__dirname, 'src/projects/projectD.html'),
+				projectE: resolve(__dirname, 'src/projects/projectE.html'),
 			},
 		},
 	},
