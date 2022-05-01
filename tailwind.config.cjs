c008f8389eaa4be96d59756c8c513a06f76ad4a1
Jordon <jordontgarcia@gmail.com>
2022-04-30 20:04:56 -0700

    Add custom imported font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@ module.exports = {
 	content: ['./index.html', './src/projects/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				dosis: ['Dosis'],
+			},
 			screens: {
 				tablet: '640px',
 				// => @media (min-width: 640px) { ... }
